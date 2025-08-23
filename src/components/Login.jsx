9ba32268af25dc9811be2f7ce55a79f47d1aaf88
nukri060 <nukri060@gmail.com>
2025-08-23 08:45:40 +0400

    feat(login): convert uncontrolled inputs to controlled with useState and handleChange

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,50 @@
+import { useState } from "react"
+
 export function Login() {
+    const [formsData, setFormsData] = useState({
+        email: "test@example.com",
+        password: "test123"
+    })
+
+    function handleSubmit(event ) {
+        event.preventDefault();
+        console.log(formsData) 
+    }
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormsData((prevData) => ({
+            ...prevData,
+            [name]: value 
+        }))
+    }
+
     return (
-        <form action="" className="text-black text-left p-10 border-2 border-blue-500">
+        <form
+            className="text-black text-left p-10 border-2 border-blue-500"
+            onSubmit={handleSubmit}
+        >
             <div className="input-field">
                 <label htmlFor="email-field">Email: </label>
-                <input type="text" id="email-field" name="email-field" className="block my-5 border-2 border-blue-500"/>
+                <input
+                    type="text"
+                    id="email-field"
+                    name="email"
+                    className="block my-5 border-2 border-blue-500"
+                    value={formsData.email}
+                    onChange={handleChange}
+                />
             </div>
             <div className="input-field">
                 <label htmlFor="password-field">Password: </label>
-                <input type="password" id="password-field" name="password-field" className="block my-5 border-2 border-blue-500"/>
+                <input
+                    type="password"
+                    id="password-field"
+                    name="password"
+                    className="block my-5 border-2 border-blue-500"
+                    value={formsData.password}
+                    onChange={handleChange}
+                />
             </div>
 
             <button type="submit">Login</button>
